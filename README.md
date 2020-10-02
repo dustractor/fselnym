1f42276550a646008417e7d20d0e0d870e51ca18
Shams Kitz <dustractor@gmail.com>
2020-10-02 01:20:27 -0500

    f6-also

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ When you choose file -> 'save' or 'save as' ( or press control+S), the text fiel
 Although I was not able to find a way to directly control the system caret in blender or to find a way to make an UI element bring the focus of the caret onto itself, I found, thanks to a comment somewhere out there, (thanks BVL) that if an operator is given a 'bl_property' attribute set to the name of a property, then that property becomes the one that is selected when the operator is displayed, so it is a two-step operation, the hotkey calls an operator set up so a string property is automatically focused, then when that operator executes via the user pressing enter or return, it sets the value of the property we weren't able to directly focus on.
 
 Control-L because that is what browsers use but in this case the mnemonic is L for "Let's have a filename"
+
+(F6 is also mapped, as an alternative to ctrl+L.)
--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 bl_info = {
     "name":"fselnym",
     "blender":(2,80,0),
+    "description":"ctrl+L or f6 during save-as dialog for popup. .blend will be appended. yes you have to hit enter three times, but still, no mouse.",
     "category":"System"
 }
 import bpy
@@ -10,18 +11,23 @@ class FSELNYM_OT_fselnym(bpy.types.Operator):
     bl_property = "s"
     s: bpy.props.StringProperty()
     def execute(self,context):
-        context.space_data.params.filename = self.s
+        t = self.s
+        if not t.endswith(".blend"):
+            t += ".blend"
+        context.space_data.params.filename = t
         return {"FINISHED"}
     def invoke(self,context,event):
         return context.window_manager.invoke_props_dialog(self)
 
 addon_keymap_data = []
+
 def register():
     addon_keymap_data.clear()
     bpy.utils.register_class(FSELNYM_OT_fselnym)
     wm = bpy.context.window_manager
     km = wm.keyconfigs.addon.keymaps.new(name="File Browser", space_type="FILE_BROWSER")
     kmi = km.keymap_items.new("fselnym.fselnym",type="L",value="PRESS",ctrl=True)
+    kmi = km.keymap_items.new("fselnym.fselnym",type="F6",value="PRESS")
     addon_keymap_data.append((km,kmi))
     
 
